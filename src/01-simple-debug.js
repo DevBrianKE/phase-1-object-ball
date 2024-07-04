b37bc7df41d4be9f4db22eee1e61327dddac368e
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-07-04 06:48:30 +0300

    console logged to view data

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -1,6 +1,14 @@
 console.log('Simple debugging example running.')
-debugger
+// debugger
 
-let x = 99
-debugger
-console.log(x)
+let x = 99;
+let y = 42;
+let d = 3
+let f = 44
+let t = 34
+let g = 29
+let b = 12  
+// debugger;
+console.log("x:", x);
+
+let oo = { foo: 42, bar: 83, "key w/ spaces": true };
